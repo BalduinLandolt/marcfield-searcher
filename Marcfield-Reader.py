597b20398d42bdc002e97156cc3fe3a786c891a8
BalduinLandolt <balduin.landolt@unibas.ch>
2019-04-19 23:35:38 +0200

    Add: cache .marc files

--- Marcfield-Reader.py
+++ Marcfield-Reader.py
@@ -1,10 +1,14 @@
 from pymarc import MARCReader
 from PyZ3950 import zoom
+import os
 
-print("loading 'all numbers'...")
-with open("all_numbers.txt") as f:
+file_name = "all_numbers.txt"
+
+print("loading: " + file_name)
+with open(file_name) as f:
     res = f.readlines()
 print("found numbers: " + str(len(res)))
+print("")
 
 f_505 = []
 f_510 = []
@@ -12,17 +16,27 @@ f_525 = []
 
 for sys_no in res:
     sys_no = sys_no.strip()
-    print("Checking: {}".format(sys_no))
-    try:
-        conn = zoom.Connection('aleph.unibas.ch', 9909)
-        conn.databaseName = 'dsv05'
-        conn.preferredRecordSyntax = 'USMARC'
 
-        query = zoom.Query('PQF', '@attr 1=1032 ' + sys_no)
-        res = conn.search(query)
-        data = bytes(res[0].data)
-    except zoom.ConnectionError:
-        print("\n!!! Error: could not connect to aleph !!!\n")
+    if os.path.isfile("cache/" + sys_no + ".marc"):
+        with open("cache/" + sys_no + ".marc", "rb") as f:
+            data = f.read()
+        print("read from cache: {}".format(sys_no))
+    else:
+        try:
+            conn = zoom.Connection('aleph.unibas.ch', 9909)
+            conn.databaseName = 'dsv05'
+            conn.preferredRecordSyntax = 'USMARC'
+
+            query = zoom.Query('PQF', '@attr 1=1032 ' + sys_no)
+            res = conn.search(query)
+            data = bytes(res[0].data)
+
+            print("loaded from aleph: {}".format(sys_no))
+
+            with open("cache/" + sys_no + ".marc", "wb") as f:
+                f.write(data)
+        except zoom.ConnectionError:
+            print("\n!!! Error: could not connect to aleph !!!\n")
 
     reader = MARCReader(bytes(data), force_utf8=True, to_unicode=True)
     mc = next(reader)
